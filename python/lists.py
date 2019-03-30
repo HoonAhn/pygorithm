d52ef57f7ac64dd2347377f63050cb329cb73f0d
Hoon Ahn <shahn801@gmail.com>
2019-03-30 17:16:10 +0900

    improved answer

--- python/lists.py
+++ python/lists.py
@@ -1,24 +1,18 @@
 # https://www.hackerrank.com/challenges/python-lists/problem
 # Started   19/03/27
 # Solved    19/03/27
+# Improved  19/03/30
 
 N = int(input())
 result = []
 for i in range(0, N):
     cmd = list(map(str, input().split()))
-    if len(cmd) == 1:
-        if cmd[0]=="print":
-            eval("print(result)")
-        else:
-            eval("result.{0}()".format(cmd[0]))
-    elif len(cmd) == 2:
-        eval("result.{0}({1})".format(cmd[0], cmd[1]))
-    elif len(cmd) == 3:
-        eval("result.{0}({1},{2})".format(cmd[0], cmd[1], cmd[2]))
-    
-    
-
-
-
-
-
+    f = cmd[0]
+    arg = cmd[1:]
+    if f=="print":
+        eval("print(result)")
+    else:
+        arg_str = ""
+        if len(arg)>0:
+            arg_str = ",".join(arg)
+        eval("result.{0}({1})".format(f,arg_str))
