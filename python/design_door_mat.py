c05fc046816d5eada086ddc67584f7d727d0c622
Sang Hoon Ahn <sanghoon.ahn@micfo.com>
2019-04-18 22:10:54 +0900

    solved

--- python/design_door_mat.py
+++ python/design_door_mat.py
@@ -1,5 +1,14 @@
 # https://www.hackerrank.com/challenges/designer-door-mat/problem
 # started 19/4/15
-# solved
+# solved 19/4/18
 
-N, M = (int)input().split()
+N, M = map(int,input().split())
+
+for i in range(N):
+    line = ''
+    if i == (int)(N/2):
+        line = '-'*(int)((M-7)/2)+'WELCOME'+'-'*(int)((M-7)/2)
+    else:
+        m = min(i*2+1, (N-i-1)*2+1)
+        line = "---" * (int)((N-m)/2) + (".|." * m) + '---' * (int)((N-m)/2)
+    print(line)
